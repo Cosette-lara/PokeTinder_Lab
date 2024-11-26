505c968d068d6d192ca2359fccefa136febc5db9
cosette-lara <cosette.lara@tecsup.edu.pe>
2024-11-26 00:08:32 -0500

    Initial commit

---
+++ app/src/main/java/com/lara/cosette/poketinder/RegisterActivity.kt
@@ -0,0 +1,15 @@
+package com.lara.cosette.poketinder
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import com.melendez.paulo.poketinder.R
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_register)
+
+    }
+}
